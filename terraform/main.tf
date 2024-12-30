f936a6f12860e76b3fc330e1a72cf351c92490b0
Taras Basiuk <BasiukTV@gmail.com>
2024-12-30 13:08:14 -0600

    Re-eanbling docker and test stages of our CI pipeline.

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,14 +41,3 @@ module "counting" {
     }]
   }
 }
-
-resource "azurerm_storage_account" "sa" {
-  name = module.naming.storage_account.name
-  resource_group_name = azurerm_resource_group.test.name
-  location = var.location
-
-  account_tier = "Standard"
-  account_replication_type = "LRS"
-
-  min_tls_version = "TLS1_1"
-}
